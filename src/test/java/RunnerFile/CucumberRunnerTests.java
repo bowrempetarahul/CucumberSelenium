ddbce7447ff8ed987f767acd78a3b6db27cc781a
Rahul <bowrempetarahul123@gmail.com>
2024-03-09 15:54:22 +0530

    added plugin pretty

--- src/test/java/RunnerFile/CucumberRunnerTests.java
+++ src/test/java/RunnerFile/CucumberRunnerTests.java
@@ -6,9 +6,9 @@ import org.testng.annotations.DataProvider;
 
 @CucumberOptions(tags = "@smoke", features = {"src/test/resources/Features/Login.feature"}, glue = {"stepDefinitions"},
         monochrome = true,
-//        plugin = { "pretty", "json:target/cucumber/report.json",
-//			"html:target/cucumber/report.html",
-        plugin = {
+        plugin = { "pretty", "json:target/cucumber/report.json",
+			"html:target/cucumber/report.html",
+//        plugin = {
         //"rerun:target/failedrerun.txt",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
 //		"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
