6e1de39b9e7c5a7b6a197da01389e69b4b9cb5e3
Rahul <bowrempetarahul123@gmail.com>
2025-03-31 15:07:50 +0530

    code fix

--- src/test/java/RunnerFile/CucumberRunnerTests.java
+++ src/test/java/RunnerFile/CucumberRunnerTests.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
-@CucumberOptions(tags = "@smoke", features = {"src/test/resources/Features/Login.feature"}, glue = {"stepDefinitions"},
+@CucumberOptions(tags = "", features = {"src/test/resources/Features/Amazon.feature"}, glue = {"stepDefinitions"},
         monochrome = true,
         plugin = { "pretty", "json:target/cucumber/report.json",
 			"html:target/cucumber/report.html",
